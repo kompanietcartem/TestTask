a64f2c835759992af696b36a2744fa2c53902337
kompanietcartem <kompanietcartem@gmail.com>
2018-04-25 22:39:42 +0300

    first commit

---
+++ Client/Client/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        private const string address = "127.0.0.1";
+        private const int port = 1234;
+
+        static void Main(string[] args)
+        {
+            Task t = StartClient();
+            t.Wait();
+        }
+
+        static async Task StartClient()
+        {
+            TcpClient client = null;
+            try
+            {
+                NetworkStream stream = await ConnectToServer(client);
+                await WorkWithServer(stream);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                StartClient();
+            }
+            finally
+            {
+                if (client != null)
+                    client.Close();
+            }
+        }
+
+        static async Task<NetworkStream> ConnectToServer(TcpClient client)
+        {
+            try
+            {
+                Console.WriteLine("Подключение к серверу...");
+                client = new TcpClient();
+                await client.ConnectAsync(address, port);
+                Console.WriteLine("Подключено");
+                return client.GetStream();
+            }
+            catch
+            {
+                Thread.Sleep(1000);
+                return await ConnectToServer(client);
+            }
+        }
+
+        static async Task WorkWithServer(NetworkStream stream)
+        {
+            while (true)
+            {
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                await stream.WriteAsync(data, 0, data.Length);
+
+                data = new byte[data.Length];
+                string response;
+                do
+                {
+                    await stream.ReadAsync(data, 0, data.Length);
+                    response = Encoding.Unicode.GetString(data);
+                }
+                while (stream.DataAvailable);
+                Console.WriteLine($"Ответ сервера: {response}");
+            }
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        private const int port = 1234;
+
+        static void Main(string[] args)
+        {
+            Task t = StartServerAsync();
+            Console.ReadLine();
+        }
+
+        static async Task StartServerAsync()
+        {
+            TcpListener server = null;
+            try
+            {
+                server = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+                server.Start();
+                Console.WriteLine("Сервер запущен");
+
+                while (true)
+                {
+                    Console.WriteLine("Ожидание нового клиента...");
+                    TcpClient client = await server.AcceptTcpClientAsync();
+                    Console.WriteLine("Клиент подключен");
+                    WorkWithClientAsync(client);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (server != null)
+                    server.Stop();
+            }
+
+        }
+
+        static async Task WorkWithClientAsync(TcpClient client)
+        {
+            NetworkStream stream = null;
+            try
+            {
+                using (stream = client.GetStream())
+                {
+                    while (client.Connected)
+                    {
+                        byte[] data = new byte[512];
+                        await stream.ReadAsync(data, 0, data.Length);
+                        string message = Encoding.Unicode.GetString(data).Trim(new char[] { '\0' });
+                        Console.WriteLine($"Получено сообщение: {message}");
+
+                        data = Encoding.Unicode.GetBytes(message.Reverse().ToArray());
+                        await stream.WriteAsync(data, 0, data.Length);
+                        Console.WriteLine("Ответ отправлен");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Dispose();
+            }
+        }
+    }
+}
